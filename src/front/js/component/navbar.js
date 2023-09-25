bac63d4a2cc4bc99c8a4439c1c0e00f14ce554c1
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2023-09-25 15:17:52 +0000

    navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,41 +1,50 @@
 import React from "react";
+import {GiHamburgerMenu } from 'react-icons/gi';
 import { Link } from "react-router-dom";
+
+import "../../styles/navbar.css";
 
 export const Navbar = () => {
 	return (
-		<nav class="navbar navbar-expand-lg bg-body-tertiary">
-			<div class="container-fluid">
-				<a class="navbar-brand bg-dark" href="#">LOGO</a>
+		<nav class="navbar navbar-expand-lg bg-light ">
+			<div class="container-fluid" id="navbarRow">
+				<a class="navbar-brand bg-dark" href="#" id="navbarBrand">
+					<img src="https://i.ibb.co/hYzgMfy/logo-Ramiro-Editado.jpg" alt="logo-Ramiro-Editado" border="0"/>
+				</a>
 				<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-					<span class="navbar-toggler-icon"></span>
+					<span class="navbar-toggler-icon">
+						<GiHamburgerMenu className="reactIcon" size="30px" />
+					</span>
 				</button>
-				<div class="collapse navbar-collapse" id="navbarSupportedContent">
-					<ul class="navbar-nav me-auto mb-2 mb-lg-0">
+				<div class="collapse navbar-collapse justify-content-end" id="navbarSupportedContent">
+					<ul class="navbar-nav ml-auto mb-2 mb-lg-0" id="navbarLinks">
 						<li class="nav-item">
 							<a class="nav-link active" aria-current="page" href="#">Home</a>
 						</li>
 						<li class="nav-item">
-							<a class="nav-link" href="#">Link</a>
+							<a class="nav-link" href="#">Clinica</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Equipa</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Serviços</a>
+						</li>
+						<li class="nav-item">
+							<a class="nav-link" href="#">Contacto</a>
 						</li>
 						<li class="nav-item dropdown">
 							<a class="nav-link dropdown-toggle" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-								Dropdown
+								Serviços
 							</a>
 							<ul class="dropdown-menu">
-								<li><a class="dropdown-item" href="#">Action</a></li>
-								<li><a class="dropdown-item" href="#">Another action</a></li>
-								<li><hr class="dropdown-divider"/></li>
-								<li><a class="dropdown-item" href="#">Something else here</a></li>
+								<li><a class="dropdown-item" href="#">Serviço 1</a></li>
+								<li><a class="dropdown-item" href="#">Serviço 2</a></li>
+								<li><a class="dropdown-item" href="#">Serviço 3</a></li>
+								<li><a class="dropdown-item" href="#">Serviço 4</a></li>
 							</ul>
 						</li>
-						<li class="nav-item">
-							<a class="nav-link disabled" aria-disabled="true">Disabled</a>
-						</li>
 					</ul>
-					<form class="d-flex" role="search">
-						<input class="form-control me-2" type="search" placeholder="Search" aria-label="Search"/>
-						<button class="btn btn-outline-success" type="submit">Search</button>
-					</form>
 				</div>
 			</div>
 		</nav>
