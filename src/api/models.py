798207439694ed7f923d14ab9f444e8980798507
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2023-10-13 16:05:31 +0000

    changes in backend

--- src/api/models.py
+++ src/api/models.py
@@ -4,9 +4,12 @@ db = SQLAlchemy()
 
 class User(db.Model):
     id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(80), unique=False, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    object = db.Column(db.String(120), unique=False, nullable=False)
+    message = db.Column(db.String(450), unique=False, nullable=False)
+
+    
 
     def __repr__(self):
         return f'<User {self.email}>'
@@ -15,5 +18,8 @@ class User(db.Model):
         return {
             "id": self.id,
             "email": self.email,
+            "name": self.name,
+            "object": self.object,
+            "message": self.message
             # do not serialize the password, its a security breach
         }
--- src/api/routes.py
+++ src/api/routes.py
@@ -16,3 +16,55 @@ def handle_hello():
     }
 
     return jsonify(response_body), 200
+
+@api.route('/create-user', methods=['POST'])
+def create_user():
+
+   data = request.get_json()
+
+   if data is None:
+       response_body = {
+           "msg": "Body should be passes with request"
+       }
+       return jsonify(response_body),400
+   
+   elif "name" not in data:
+        response_body = {
+           "msg": "Email doesnt exist in the request"
+       }
+        return jsonify(response_body),400
+   
+   elif "email" not in data:
+        response_body = {
+           "msg": "Email doesnt exist in the request"
+       }
+        return jsonify(response_body),400
+
+   elif "object" not in data:
+        response_body = {
+           "msg": "Object doesnt exist in the request"
+       }
+        return jsonify(response_body),400
+   
+   elif "message" not in data:
+        response_body = {
+           "msg": "Message doesnt exist in the request"
+       }
+        return jsonify(response_body),400
+   
+   new_user= User(name = data["name"], email = data["email"], object= data["object"], message=data["message"])
+   db.session.add(new_user)   
+   db.session.commit()
+
+   return jsonify({"msg": "user has been added"}), 200
+
+@api.route('/users',methods=['GET'])
+def get_all_users():
+    all_users = User.query.all()
+    all_users= list(map(lambda x: x.serialize(),all_users))
+    return jsonify(all_users),200
+
+@api.route('/user/<int:id>',methods=["GET"])
+def get_single_user(id):
+    user = User.query.get(id)
+    return jsonify(user.serialize()), 200
