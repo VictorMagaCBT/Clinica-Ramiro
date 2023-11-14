d247138c15c97526ebc3daebcc5c923ac7cdd779
VICTOR MAGALHAES <126924738+VictorMagaCBT@users.noreply.github.com>
2023-11-14 14:36:52 +0000

    styles of pages defined and responsiveness

--- src/front/js/pages/oral.js
+++ src/front/js/pages/oral.js
@@ -11,7 +11,12 @@ const Oral = () => {
         <div className="content-Oral">
             <h1 className="titleOral mt-5 text-center">Cirurgia Oral</h1>
             <div className="introductionOral">
-                <p className="introduction-text-Oral text-center">Às vezes, nossa boca pede uma intervenção. A cirurgia oral é a resposta, um campo onde a ciência se torna arte, resolvendo questões de saúde e estética bucais.</p>
+                <p className="introduction-text-Oral text-center">
+                    Às vezes, nossa boca pede uma intervenção. 
+                        <br/><br/>
+                    A cirurgia oral é a resposta, um campo onde a ciência se torna arte, resolvendo questões 
+                    de saúde e estética bucais.
+                </p>
                 <div className="introducao-image-Oral">
                 <img src="https://i.ibb.co/NNRWCLx/Imagem-15-recortada.jpg" alt="Imagem-15-recortada" border="0"/>
                 </div>
@@ -21,19 +26,28 @@ const Oral = () => {
                     <img src="https://i.ibb.co/28jrwH2/raiox.jpg" alt="raiox" border="0"/>
                 </div>
                 <p className="oral-text text-center">
-                Imagine-a como um laboratório, onde os especialistas dentais são os pesquisadores e você, o paciente. Aqui, extração de dentes, enxertos ósseos e remoção de lesões ganham soluções. É um processo estruturado, onde cada etapa é meticulosamente planejada para garantir um sorriso saudável.
+                    Imagine-a como um laboratório, onde os especialistas dentais são os pesquisadores e você, o paciente. 
+                        <br/><br/>
+                    Aqui, extração de dentes, enxertos ósseos e remoção de lesões ganham soluções. 
+                        <br/><br/>
+                    É um processo estruturado, onde cada etapa é meticulosamente planejada para garantir um sorriso saudável.
                 </p>
             </div>
-            <div className="bottom-oral-text text-center">
+            <div className="bottomOralText text-center">
                 <p>
-                Essas intervenções não são apenas reparos; são melhorias, com o objetivo de aprimorar saúde e bem-estar. Ao escolher essa abordagem, você está moldando um novo capítulo para sua saúde bucal, com os cirurgiões orais como os facilitadores, ajustando e otimizando cada detalhe.
+                    Essas intervenções não são apenas reparos; são melhorias, com o objetivo de aprimorar saúde e bem-estar. 
+                        <br/><br/>
+                    Ao escolher essa abordagem, você está moldando um novo capítulo para sua saúde bucal, com os cirurgiões orais 
+                    como os facilitadores, ajustando e otimizando cada detalhe.
                 </p>
                 <p>
-                A cirurgia oral é a promessa de um sorriso renovado e a garantia de uma boca funcional. É um caminho de progresso, onde a confiança ganha destaque e a vitalidade é o objetivo. 
+                    A cirurgia oral é a promessa de um sorriso renovado e a garantia de uma boca funcional. 
+                        <br/><br/>
+                    É um caminho de progresso, onde a confiança ganha destaque e a vitalidade é o objetivo. 
                 </p>
             </div>
-            <div className="moreInfo">
-                <button className="btn-more-info" 
+            <div className="moreInfoOral">
+                <button className="btnOral" 
                 data-toggle="modal" 
                 data-target="#exampleModalCenter"
                 onClick={() => navigate("/contactos")}>Quer saber mais...?</button>
--- src/front/js/pages/reabilitaçãoSobreImplantes.js
+++ src/front/js/pages/reabilitaçãoSobreImplantes.js
@@ -8,18 +8,36 @@ const Implantes = () => {
     const navigate = useNavigate();
     return (
         <div className="content-implantes">
-            <h1 className="cirurgiaImplantes mt-5 text-center">Cirurgia Ortognatica</h1>
-            <div className="introduction">
-                <div className="introducao-image">
+            <h1 className="cirurgiaImplantes mt-5 text-center">Cirurgia de Reabilitação Sobre Implantes</h1>
+            <div className="introductionImplantes">
+                <div className="introductionImageImplantes">
                 <img src="https://i.ibb.co/ZTpvHVF/sorriso-velhote.jpg" alt="sorriso-velhote" border="0"/>
                 </div>
-                <p className="introduction-text">Em um mundo onde a inovação se encontra com a odontologia, a cirurgia de reabilitação com implantes surge como uma dádiva para aqueles que buscam restaurar sua saúde bucal e a confiança em seu sorriso. É uma jornada meticulosa, onde a ciência se une à precisão cirúrgica para reconstruir a harmonia oral.</p>
-            </div>
-            <div className="dentadura-part">
-                <p className="dentadura-text">Imagine implantes dentários como pilares sólidos, incorporados estrategicamente em sua mandíbula. Eles são a base, a fundação para futuras reabilitações. A cirurgia é um ato de maestria, onde esses implantes são habilmente colocados, proporcionando um alicerce robusto para os novos dentes que estão por vir.
-                    Essa intervenção é mais do que a criação de um novo sorriso; é a restauração da funcionalidade perdida e a reinvenção da estética dental. É um compromisso com a longevidade, onde os implantes oferecem estabilidade e durabilidade. A reabilitação traz de volta a alegria de comer, sorrir e falar com naturalidade.
+                <p className="introductionText text-center">
+                    Em um mundo onde a inovação se encontra com a odontologia, a cirurgia de reabilitação com 
+                    implantes surge como uma dádiva para aqueles que buscam restaurar sua saúde bucal e a 
+                    confiança em seu sorriso.
+                        <br/><br/>
+                    É uma jornada meticulosa, onde a ciência se une à precisão cirúrgica para reconstruir a harmonia oral.
                 </p>
-                <div className="dentadura-image">
+            </div>
+            <div className="dentaduraPart">
+                <p className="dentaduraText">
+                    Imagine implantes dentários como pilares sólidos, incorporados estrategicamente em sua mandíbula. 
+                        <br/><br/>
+                    Eles são a base, a fundação para futuras reabilitações. 
+                        <br/><br/>
+                    A cirurgia é um ato de maestria, onde esses implantes são habilmente colocados, proporcionando um 
+                    alicerce robusto para os novos dentes que estão por vir.
+                        <br/><br/>
+                    Essa intervenção é mais do que a criação de um novo sorriso; é a restauração da funcionalidade 
+                    perdida e a reinvenção da estética dental. 
+                        <br/>
+                    É um compromisso com a longevidade, onde os implantes oferecem estabilidade e durabilidade. 
+                        <br/><br/>
+                    A reabilitação traz de volta a alegria de comer, sorrir e falar com naturalidade.
+                </p>
+                <div className="dentaduraImage">
                     <img src="https://i.ibb.co/7tnhV1F/implantes-em-3d.jpg" alt="implantes-em-3d" border="0"/>
                 </div>
             </div>
@@ -36,16 +54,24 @@ const Implantes = () => {
                 </div>
                
             </div>
-            <div className="final-text">
+            <div className="bottomTextImplantes">
                 <p className="cirurgia-text2">
-                    Ao optar por essa transformação, você está abrindo as portas para uma vida sem limitações, sem preocupações com a integridade de sua dentição. A cirurgia de reabilitação com implantes é uma promessa de uma nova fase, onde cada mordida é um passo firme em direção à confiança e ao bem-estar.
+                    Ao optar por essa transformação, você está abrindo as portas para uma vida sem limitações, 
+                    sem preocupações com a integridade de sua dentição. 
+                        <br/><br/>
+                    A cirurgia de reabilitação com implantes é uma promessa de uma nova fase, onde cada mordida 
+                    é um passo firme em direção à confiança e ao bem-estar.
                 </p>
                 <p className="cirurgia-text3">
-                    A ciência da odontologia evolui e a cirurgia de reabilitação com implantes é um testemunho dessa evolução. É a busca constante por uma melhor qualidade de vida, onde a inovação se une à experiência para criar sorrisos que perduram.
+                    A ciência da odontologia evolui e a cirurgia de reabilitação com implantes é um testemunho
+                    dessa evolução. 
+                        <br/><br/>
+                    É a busca constante por uma melhor qualidade de vida, onde a inovação se une à experiência para
+                     criar sorrisos que perduram.
                 </p> 
             </div>
-            <div className="moreInfo">
-                <button className="btn-more-info" 
+            <div className="moreInfoImplantes">
+                <button className="btnImplantes" 
                 data-toggle="modal" 
                 data-target="#exampleModalCenter"
                 onClick={() => navigate("/contactos")}>Quer saber mais...?</button>
